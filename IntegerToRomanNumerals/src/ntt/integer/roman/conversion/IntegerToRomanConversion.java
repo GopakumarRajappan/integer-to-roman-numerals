43c1e6a1e3d1d66dde73ba417266f0d40e5f07fa
GopakumarRajappan <gopakumar.rajappan01@gmail.com>
2021-07-03 16:50:21 +1000

    Exception Handling added for invalid user input and user input scanner.

--- IntegerToRomanNumerals/src/ntt/integer/roman/conversion/IntegerToRomanConversion.java
+++ IntegerToRomanNumerals/src/ntt/integer/roman/conversion/IntegerToRomanConversion.java
@@ -26,107 +26,128 @@ public class IntegerToRomanConversion {
 		integerRomanMap.put(500, "D");
 		integerRomanMap.put(1000, "M");
 		
-		System.out.print("Enter Integer to be converted to Roman numeral: ");
-		Scanner scan = new Scanner(System.in);		
-		int userIntegerInput = scan.nextInt();
-
-		String romanNumeral = "";
-		
-
 		/**
-	    * while loop is used to exit the loop based on exit condition.
-	    * Valid user input is verified against the standard "key" mentioned in
-	    * the integerRomanMap. Also special cases for each valid key is considered.
-		**/		
-		int quotient, remainder;
-		
-		while (userIntegerInput != 0) {			
-			if (userIntegerInput/1000 != 0) {
-				quotient = userIntegerInput / 1000;
-				remainder = userIntegerInput % 1000;				
-				for (int iterator=0; iterator < quotient; iterator ++) {
-					romanNumeral+= integerRomanMap.get(1000);				
-				}				
-				userIntegerInput = remainder;	
-			}else if (userIntegerInput >=900) {
-				quotient = userIntegerInput/900;
-				remainder = userIntegerInput % 900;
-				romanNumeral = romanNumeral + integerRomanMap.get(100) + integerRomanMap.get(1000);
-				userIntegerInput = remainder;
-			} else if (userIntegerInput/500 != 0){
-				quotient = userIntegerInput/500;
-				remainder = userIntegerInput%500;
-				romanNumeral+= integerRomanMap.get(500);				
-				userIntegerInput = remainder;
-			}
-			else if (userIntegerInput >= 400){
-				quotient = userIntegerInput/400;
-				remainder = userIntegerInput%400;
-				romanNumeral+=  integerRomanMap.get(100) + integerRomanMap.get(500);	
-				userIntegerInput = remainder;
-			}
-					
-			else if (userIntegerInput/100 != 0) {
-				quotient = userIntegerInput/100;
-				remainder = userIntegerInput%100;				
-				for (int iterator=0; iterator < quotient; iterator ++) {
-					romanNumeral+= integerRomanMap.get(100);				
-				}				
-				userIntegerInput = remainder;			
-			}else if (userIntegerInput >=90) {
-				quotient = userIntegerInput/90;
-				remainder = userIntegerInput%90;
-				romanNumeral = romanNumeral + integerRomanMap.get(10) + integerRomanMap.get(100);
-				userIntegerInput = remainder;
-			} else if (userIntegerInput/50 != 0){
-				quotient = userIntegerInput/50;
-				remainder = userIntegerInput%50;
-
-				romanNumeral+= integerRomanMap.get(50);				
-				userIntegerInput = remainder;
-			}
-			else if (userIntegerInput >=40){
-				quotient = userIntegerInput/40;
-				remainder = userIntegerInput%40;				
-				romanNumeral+=  integerRomanMap.get(10) + integerRomanMap.get(50);	
-				userIntegerInput = remainder;
-			}
-						
-			else if (userIntegerInput/10 != 0) {
-				quotient = userIntegerInput/10;
-				remainder = userIntegerInput%10;				
-				for (int iterator=0; iterator < quotient; iterator ++) {
-					romanNumeral+= integerRomanMap.get(10);				
-				}				
-				userIntegerInput = remainder;			
-			}else if (userIntegerInput >=9) {
-				quotient = userIntegerInput/9;
-				remainder = userIntegerInput%9;
-				romanNumeral+= integerRomanMap.get(1) + integerRomanMap.get(10);
-				userIntegerInput = remainder;
-			} else if (userIntegerInput/5 != 0){
-				quotient = userIntegerInput/5;
-				remainder = userIntegerInput%5;
-				romanNumeral+= integerRomanMap.get(5);				
-				userIntegerInput = remainder;
-			}
-			else if (userIntegerInput > 4){
-				quotient = userIntegerInput/4;
-				remainder = userIntegerInput%4;				
-				romanNumeral+=  integerRomanMap.get(1) + integerRomanMap.get(5);	
-				userIntegerInput = remainder;
-			}
-			
-			else if (userIntegerInput/1 != 0) {
-				quotient = userIntegerInput/1;
-				remainder = userIntegerInput%1;
-				
-				for (int iterator=0; iterator < quotient; iterator ++) {
-					romanNumeral+= integerRomanMap.get(1);				
-				}				
-				userIntegerInput = remainder;			
-			}
+		 * Accepting user input and closing scanner after use.
+		 */
+		System.out.print("Enter Integer to be converted to Roman numeral: ");
+		Scanner scan = new Scanner(System.in);
+		int userIntegerInput;
+		try{
+			userIntegerInput = scan.nextInt();
+		}finally {
+			scan.close();
 		}
-		System.out.println("Roman numeral equivalent of the given input is: " + romanNumeral);
+		
+		/**
+		 * Throw exception for invalid inputs.
+		 */
+		try {
+			if (userIntegerInput <1 || userIntegerInput > 5000) {
+				throw new Exception();
+			}
+
+			String romanNumeral = "";
+			
+
+			/**
+		    * while loop is used to exit the loop based on exit condition.
+		    * Valid user input is verified against the standard "key" mentioned in
+		    * the integerRomanMap. Also special cases for each valid key is considered.
+			**/		
+			int quotient, remainder;
+			
+			while (userIntegerInput != 0) {			
+				if (userIntegerInput/1000 != 0) {
+					quotient = userIntegerInput / 1000;
+					remainder = userIntegerInput % 1000;				
+					for (int iterator=0; iterator < quotient; iterator ++) {
+						romanNumeral+= integerRomanMap.get(1000);				
+					}				
+					userIntegerInput = remainder;	
+				}else if (userIntegerInput >=900) {
+					quotient = userIntegerInput/900;
+					remainder = userIntegerInput % 900;
+					romanNumeral = romanNumeral + integerRomanMap.get(100) + integerRomanMap.get(1000);
+					userIntegerInput = remainder;
+				} else if (userIntegerInput/500 != 0){
+					quotient = userIntegerInput/500;
+					remainder = userIntegerInput%500;
+					romanNumeral+= integerRomanMap.get(500);				
+					userIntegerInput = remainder;
+				}
+				else if (userIntegerInput >= 400){
+					quotient = userIntegerInput/400;
+					remainder = userIntegerInput%400;
+					romanNumeral+=  integerRomanMap.get(100) + integerRomanMap.get(500);	
+					userIntegerInput = remainder;
+				}
+						
+				else if (userIntegerInput/100 != 0) {
+					quotient = userIntegerInput/100;
+					remainder = userIntegerInput%100;				
+					for (int iterator=0; iterator < quotient; iterator ++) {
+						romanNumeral+= integerRomanMap.get(100);				
+					}				
+					userIntegerInput = remainder;			
+				}else if (userIntegerInput >=90) {
+					quotient = userIntegerInput/90;
+					remainder = userIntegerInput%90;
+					romanNumeral = romanNumeral + integerRomanMap.get(10) + integerRomanMap.get(100);
+					userIntegerInput = remainder;
+				} else if (userIntegerInput/50 != 0){
+					quotient = userIntegerInput/50;
+					remainder = userIntegerInput%50;
+
+					romanNumeral+= integerRomanMap.get(50);				
+					userIntegerInput = remainder;
+				}
+				else if (userIntegerInput >=40){
+					quotient = userIntegerInput/40;
+					remainder = userIntegerInput%40;				
+					romanNumeral+=  integerRomanMap.get(10) + integerRomanMap.get(50);	
+					userIntegerInput = remainder;
+				}
+							
+				else if (userIntegerInput/10 != 0) {
+					quotient = userIntegerInput/10;
+					remainder = userIntegerInput%10;				
+					for (int iterator=0; iterator < quotient; iterator ++) {
+						romanNumeral+= integerRomanMap.get(10);				
+					}				
+					userIntegerInput = remainder;			
+				}else if (userIntegerInput >=9) {
+					quotient = userIntegerInput/9;
+					remainder = userIntegerInput%9;
+					romanNumeral+= integerRomanMap.get(1) + integerRomanMap.get(10);
+					userIntegerInput = remainder;
+				} else if (userIntegerInput/5 != 0){
+					quotient = userIntegerInput/5;
+					remainder = userIntegerInput%5;
+					romanNumeral+= integerRomanMap.get(5);				
+					userIntegerInput = remainder;
+				}
+				else if (userIntegerInput > 4){
+					quotient = userIntegerInput/4;
+					remainder = userIntegerInput%4;				
+					romanNumeral+=  integerRomanMap.get(1) + integerRomanMap.get(5);	
+					userIntegerInput = remainder;
+				}
+				
+				else if (userIntegerInput/1 != 0) {
+					quotient = userIntegerInput/1;
+					remainder = userIntegerInput%1;
+					
+					for (int iterator=0; iterator < quotient; iterator ++) {
+						romanNumeral+= integerRomanMap.get(1);				
+					}				
+					userIntegerInput = remainder;			
+				}
+			}
+			System.out.println("Roman numeral equivalent of the given input is: " + romanNumeral);
+		}catch (Exception exception) {
+			System.out.println("Enter a valid number from 1 to 5000");
+		}
 	}
+        
+
 }
